36fa69c6a389fdf788f388d2c6fd9a6939239e26
Zayas Rios Moises Daniel <119543624+ZayasDaniel@users.noreply.github.com>
2023-03-07 12:38:24 -0600

    Delete Tarea 6 Clases directory

--- Tarea 6 Clases/Monitor/MonitorMain.java
+++
@@ -1,9 +0,0 @@
-public class MonitorMain {
-    public static void main(String[] args) {
-        Monitor mon = new Monitor(27.4, "Samsung", "SM10", "LED", 6700.0);
-        System.out.println(mon);
-        mon.encender();
-        mon.apagar();
-        mon.proyector();
-    }
-}
